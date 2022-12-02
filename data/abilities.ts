d2b13109ba39d611954445e4811b8ef1b3532e18
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-12-02 10:08:51 +0700

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -1,1 +1,19 @@
 export const Abilities: {[abilityid: string]: AbilityData} = {
+	choicewords: {
+		onBasePowerPriority: 7,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.flags['sound']) {
+				this.debug('Choice Words boost');
+				return this.chainModify([0x14CD, 0x1000]);
+			}
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.category === 'Special') {
+				this.debug('Choice Words boost');
+				return this.chainModify(1.25);
+			}
+		},
+		name: "Choice Words",
+		num: 1,
+	},
+};
