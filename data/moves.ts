7f9f9eb1cdada0f8f2e3f6913b2c6ba15778fcd8
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-12-02 21:58:22 +0700

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -1,5 +1,5 @@
 export const Moves: {[moveid: string]: MoveData} = {
-	standardpunc: {
+	standardpunch: {
 		num: 1,
 		accuracy: 100,
 		basePower: 0,
@@ -21,9 +21,38 @@ export const Moves: {[moveid: string]: MoveData} = {
 		name: "Standard Punch",
 		pp: 15,
 		priority: 0,
-		flags: {protect: 1, mirror: 1},
+		flags: {contact: 1, protect: 1, mirror: 1},
 		secondary: null,
 		target: "normal",
 		type: "Ordinary",
 		contestType: "Clever",
 	},
+	standardshriek: {
+		num: 1,
+		accuracy: 100,
+		basePower: 0,
+		onModifyMove(move, pokemon) {
+			const i = this.random(100);
+			if (i < 20) {
+				move.basePower = 80;
+			} else if (i < 40) {
+				move.basePower = 85;
+			} else if (i < 60) {
+				move.basePower = 90;
+			} else if (i < 80) {
+				move.basePower = 95;
+			} else {
+				move.basePower = 100;
+			}
+		},
+		category: "Special",
+		name: "Standard Punch",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, sound: 1},
+		secondary: null,
+		target: "normal",
+		type: "Ordinary",
+		contestType: "Clever",
+	},
+};
