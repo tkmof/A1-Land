ef1ce8bf5b63893c4f667c9bdce934466e5de408
t km of 11 209 <92902850+tkmof@users.noreply.github.com>
2022-12-02 22:05:31 +0700

    Create learnsets.ts

---
+++ data/learnsets.ts
@@ -0,0 +1,32 @@
+export const Learnsets: {[speciesid: string]: LearnsetData} = {
+   khanhan: {
+      learnset: {
+         standardpunch: ["8L1"],
+         standardshriek: ["8L1"],
+      },
+   },   
+   tuananh: {
+      learnset: {
+         standardpunch: ["8L1"],
+         standardshriek: ["8L1"],
+      },
+   },
+   leminh: {
+      learnset: {
+         standardpunch: ["8L1"],
+         standardshriek: ["8L1"],
+      },
+   },
+   baoquyen: {
+      learnset: {
+         standardpunch: ["8L1"],
+         standardshriek: ["8L1"],
+      },
+   },
+   hoangkhoi: {
+      learnset: {
+         standardpunch: ["8L1"],
+         standardshriek: ["8L1"],
+      },
+   },
+};
